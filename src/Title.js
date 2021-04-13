74102d709813c1689ea705675d1176ac1cf373c3
ImerG12 <77695762+ImerG12@users.noreply.github.com>
2021-04-13 10:34:07 -0700

    Step

---
+++ src/Title.js
@@ -0,0 +1,14 @@
+// src/Title.
+import './Title.css';
+import React from 'react'
+
+function Title() {
+  return (
+    <div>
+      <h1>SFPOPOS</h1>
+        //  <div className="Title-Subtitle">San Franciscos Privately Owned Public Spaces</div>
+    </div>
+  )
+}
+
+export default Title
